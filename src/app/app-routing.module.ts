794156d8fe827d477afba3dafe247e6fe81f7ed6
JacoboZabala <jujazabala@gmail.com>
2023-04-11 17:00:05 -0500

    Cap25

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio.component';
 import { LibrosComponent } from './Libros/libros.component';
+import { RegistrarComponent } from './seguridad/registrar/registrar.component';
+import { LoginComponent } from './seguridad/login/login.component';
 
 const routes: Routes = [
   { path:'', component: InicioComponent},
   { path:'libros', component: LibrosComponent},
+  { path:'registrar', component: RegistrarComponent},
+  { path:'login', component: LoginComponent}
 ];
 
 @NgModule({
